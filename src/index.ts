5c4b4daec18dc13077dbfc187b6dc141d3fb310b
JasonLuis <jason20luis@gmail.com>
2021-10-15 22:26:39 -0300

    chore: implemented cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import * as express from "express";
 import './database/connect';
+import * as cors from "cors";
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3333);
