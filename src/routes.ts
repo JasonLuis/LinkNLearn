3a5be4d2eeb1e2ebe35c3cac55ebede767739d18
Alexandre Santos <sasle57@gmail.com>
2021-10-17 00:12:13 -0300

    student photo upload

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,19 @@ import { createFeedBack, listFeedbackByCourse } from './app/controller/Feedback/
 
 const routes = Router();
 
+const multer = require('multer');
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, callback) => {
+            callback(null, 'uploads/');
+        },
+        filename: (req, file, callback) => {
+            const type = file.mimetype.split('/')[1];
+            callback(null, file.originalname + '-' + Date.now() + '.' + type);
+        }
+    })
+});
+
 routes.get('/', (request: Request, response: Response) => {
     return response.json({ message: 'Bem vindo a LinkLearn' });
 });
@@ -25,6 +38,10 @@ routes.post('/student/buy', authUserMiddleware, buyCourses);
 routes.post('/student/listCourses', authUserMiddleware, listAllByCourses);
 routes.post('/student/getById', authUserMiddleware, getStudentById);
 routes.post('/student/forgot-password', forgotPassword);
+routes.post('/student/upload/profile', authUserMiddleware, upload.single('photo'), function (req, res, next) {
+    return res.status(200);
+});
+
 
 //Rotas para Tabela Plans
 routes.get('/plan/listAll', listPlans);
@@ -49,4 +66,5 @@ routes.post('/courses/create', authUserMiddleware, saveCourse);
 // Feedback
 routes.post('/course/listAllFeedback', listFeedbackByCourse); //listar
 routes.post('/course/feedback', authUserMiddleware, createFeedBack);//criar
+
 export default routes;
